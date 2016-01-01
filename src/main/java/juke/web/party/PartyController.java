6aff26bab9d387fe90e543d459a6c51112664bbc
frubbs <rafaelhss@gmail.com>
2016-01-01 14:05:14 -0200

    dfghjkl

--- src/main/java/juke/web/party/PartyController.java
+++ src/main/java/juke/web/party/PartyController.java
@@ -284,8 +284,8 @@ public class PartyController {
             ImageWriter writer = (ImageWriter) ImageIO.getImageWritersByFormatName("jpeg").next();
 
             ImageWriteParam param = writer.getDefaultWriteParam();
-            param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-            param.setCompressionQuality(0.2f); // Change this, float between 0.0 and 1.0
+            //param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+            //param.setCompressionQuality(0.2f); // Change this, float between 0.0 and 1.0
 
             writer.setOutput(ImageIO.createImageOutputStream(os));
             writer.write(null, new IIOImage(originalImage, null, null), param);
